f29b7057bffeb0c63111c525215e0d0761c3a960
Alex <alexvatti@gmail.com>
2020-05-06 11:00:16 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,4 +5,6 @@ void print()
 	std::cout << "Source Modifed" << std::endl;
 	std::cout << "For Build Trigger!" << std::endl;
 	std::cout << "Testing!" << std::endl;
+	std::cout << "Testing!" << std::endl;
+	
 }
