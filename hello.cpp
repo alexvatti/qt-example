afb97ee22ef70d2d3c3e9c7090c70dbaa15591d9
Alexander Vatti <alexander.vatti@jadaktech.com>
2020-05-06 09:57:46 +0530

    edit the hello.cpp for Jenkins Build Trigger on SCM

--- hello.cpp
+++ hello.cpp
@@ -2,4 +2,7 @@
 void print()
 {
 	std::cout << "QMake!" << std::endl;
+	std::cout << "Source Modifed" << std::endl;
+	std::cout << "For Build Trigger!" << std::endl;
+	std::cout << "Testing!" << std::endl;
 }
